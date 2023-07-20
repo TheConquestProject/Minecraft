cc5d6abef3305f46466c166534bef61e4af876bc
TheConquestProject <140089277+TheConquestProject@users.noreply.github.com>
2023-07-20 16:59:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value,
-  };
-
-    const serviceID = "service_7tro358";
-    const templateID = "template_ejxp884";
-
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
-
-    })
-    .catch(err=>console.log(err));
-
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    
+    var templateParams = {
+        name: name,
+        email: email,
+        message: message,
+        reply_to: email
+    };
+    
+    emailjs.sendForm("service_64d12im", "template_ejxp884", templateParams)
+        .then(function(response) {
+            // Remplacez le message de console.log par une alerte de succès
+            alert("Email envoyé avec succès!");
+            // Vous pouvez également ajouter d'autres actions à effectuer après le succès de l'envoi.
+        })
+        .catch(function(error) {
+            // Remplacez le message de console.log par une alerte d'erreur
+            alert("Erreur lors de l'envoi de l'email");
+            // Vous pouvez également ajouter d'autres actions à effectuer en cas d'erreur d'envoi.
+        });
 }
